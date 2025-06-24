f11a9eeff7d7b314e76ef1d0a53a609beb1294b2
rusonnn <rushan2003@gmail.com>
2025-06-25 00:30:43 +0530

    Update 2048.py

--- 2048/2048.py
+++ 2048/2048.py
@@ -6,8 +6,8 @@ st.set_page_config(page_title="2048 Game", layout="centered")
 st.markdown("""
    <style>
     .tile {
-        width: 100%;
-        height: 100%;
+        width: 10%;
+        height: 10%;
         display: flex;
         align-items: center;
         justify-content: center;
